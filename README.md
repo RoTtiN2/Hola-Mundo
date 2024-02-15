e1199db850d36b7ff095874ee4a7f2d23db67cc5
RoTtiN2 <160083533+RoTtiN2@users.noreply.github.com>
2024-02-15 02:01:57 +0000

    Version 0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Hola-Mundo
+
+## practica 1 
+para generar el codigo Hola mundo ejecutar la siguiente instruccion.
+> g++ main.cpp -o programa
+
+para ejecutarlo usar
+> ./programa
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hola mundo"
+         << "23110166" << endl;
+
+    return 0;
+}
