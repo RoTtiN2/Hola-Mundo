1776dcbd7bf54f2ecacf498335d4e0ec5b0953c8
RoTtiN2 <160083533+RoTtiN2@users.noreply.github.com>
2024-03-07 02:04:52 +0000

    version 4

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <Alimento.hpp>
 #include <Mascota.hpp>
+#include <Dibujo.hpp>
+#include <Animacion.hpp>
+#include <list>
 using namespace std;
 
 int main(int argc, char const *argv[])
@@ -8,15 +11,27 @@ int main(int argc, char const *argv[])
     Mascota firulais;
     Alimento carne(10);
 
- firulais.Comer(carne);
+    firulais.Comer(carne);
 
- firulais.Jugar();
- firulais.Jugar();
- firulais.Jugar();
+    firulais.Jugar();
+    firulais.Jugar();
+    firulais.Jugar();
 
- cout
- <<"vida de firulais"
- << firulais.LeerVida()
- << endl;
+    cout
+        << "vida de firulais"
+        << firulais.LeerVida()
+        << endl;
+
+    Dibujo dibujo("./assets/dragon.txt");
+    Dibujo dibujo2("./assets/craneo.txt");
+
+    list<Dibujo> dibujos;
+    dibujos.push_back(dibujo);
+    dibujos.push_back(dibujo2);
+
+    Animacion animacion(dibujos);
+    animacion.Reproducir();
+
+
     return 0;
 }
